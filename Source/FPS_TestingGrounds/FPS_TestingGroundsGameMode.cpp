538c868c501b4e76691e122602823c9db53051b0
Walid_CHEKKOURI <virtualworld45@gmail.com>
2020-09-16 11:27:24 +0100

    Refactor TP and FP files

--- Source/FPS_TestingGrounds/FPS_TestingGroundsGameMode.cpp
+++ Source/FPS_TestingGrounds/FPS_TestingGroundsGameMode.cpp
@@ -9,7 +9,7 @@ AFPS_TestingGroundsGameMode::AFPS_TestingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behavior/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Behavior/Character_BP"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
