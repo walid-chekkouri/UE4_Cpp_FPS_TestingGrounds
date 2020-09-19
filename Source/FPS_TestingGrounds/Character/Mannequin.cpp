70f42a14748830dc4f432e73ffbc83dbed31c702
Walid_CHEKKOURI <virtualworld45@gmail.com>
2020-09-19 11:59:38 +0100

    Create and play fire montage

--- Source/FPS_TestingGrounds/Character/Mannequin.cpp
+++ Source/FPS_TestingGrounds/Character/Mannequin.cpp
@@ -44,7 +44,7 @@ void AMannequin::BeginPlay()
 
 	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
 	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
-	Gun->AnimInstance = Mesh1P->GetAnimInstance();
+	Gun->AnimInstance = GetMesh()->GetAnimInstance();
 
 	if (InputComponent != nullptr)
 	{
